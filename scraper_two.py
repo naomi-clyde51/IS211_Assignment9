784e8e7157c4a46f56495a1e50b59fb7c661e503
Naomi Clyde <49344161+naomi-clyde51@users.noreply.github.com>
2026-05-23 21:11:01 -0400

    Implement SSL context for Wikipedia scraping
    Added SSL context to bypass HTTP 403 error when scraping Wikipedia.

--- scraper_two.py
+++ scraper_two.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import ssl
 
+#LINK: "https://en.wikipedia.org/wiki/Presidents_of_the_United_States"
+#Wikipedia does not let me scrape their data without giving 'HTTP ERROR 403: Forbidden'
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+tables = pd.read_html("https://en.wikipedia.org/wiki/Presidents_of_the_United_States")
+
+len(tables)
+tables[1]
 if __name__ == "__main__":
     print("Running scrapper two...")
